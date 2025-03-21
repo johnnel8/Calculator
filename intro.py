9db77b21b344a5a5712991e4111fc229a96f50ac
johnnel <johnnelngure888@.com>
2025-03-21 15:22:06 +0300

    first commit

--- intro.py
+++
@@ -1,6 +0,0 @@
-# Variables hold data
-age = 25 # int (integer)
-height =5.75   #float(decimal)
-name = "johnnel" #str (string)
-is_student =True  #bool (boolean)
-
--- testnumberq.py
+++
@@ -1,9 +0,0 @@
-# If-elif-else statements  
-num = 10  
-
-if num > 0:  
-    print("Positive number")  
-elif num == 0:  
-    print("Zero")  
-else:  
-    print("Negative number")
